175057ec9c21eb5040b672c7af486f00baec6023
Mojtaba Michael <mojimich2015@gmail.com>
2024-05-15 22:57:16 +0300

    WIP - prices package

--- prices/prices.go
+++ prices/prices.go
@@ -1,5 +1,7 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
@@ -11,4 +13,12 @@ func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
 		TaxRate:     taxRate,
 		InputPrices: []float64{10, 20, 30},
 	}
+}
+
+func (job TaxIncludedPriceJob) Process() {
+	result := make(map[string]float64)
+	for _, price := range job.InputPrices {
+		result[fmt.Sprint(price)] = price * (1 + job.TaxRate)
+	}
+	result[taxRate] = taxIncludedPrices
 }
