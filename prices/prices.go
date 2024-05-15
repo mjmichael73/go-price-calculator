b6d89ce7f9cae7150b24b71ba108ee81f77126ba
Mojtaba Michael <mojimich2015@gmail.com>
2024-05-15 20:03:54 +0300

    Added TaxIncludedPRiceJob constructor

--- prices/prices.go
+++ prices/prices.go
@@ -5,3 +5,10 @@ type TaxIncludedPriceJob struct {
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
+
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		TaxRate:     taxRate,
+		InputPrices: []float64{10, 20, 30},
+	}
+}
